685e02876a97f5003957318115e390ba37f45866
Eduardo Camillo <eduardo.camillo@agross.com.br>
2026-04-27 18:04:25 -0300

    include home tests

--- src/app.js
+++ src/app.js
@@ -6,6 +6,10 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 
+app.get('/',(req,res) =>{
+  res.json({mensagem: 'Ta on!'})
+})
+
 app.get('/api/test', (req, res) => {
   res.json({ mensagem: 'API funcionando 🚀' });
 });
